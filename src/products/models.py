fca1538cd7c3f64d6872a0a830da0d19547004d8
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-10-06 12:13:39 +0530

    Added Templates

--- src/products/models.py
+++ src/products/models.py
@@ -7,4 +7,5 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10000, decimal_places=2)
-    summary = models.TextField()
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField()
