447493835cc39b413f02059ba9afb0d054b6b649
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-10-07 12:26:26 +0530

    Intro to Forms

--- src/products/models.py
+++ src/products/models.py
@@ -8,4 +8,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10000, decimal_places=2)
     summary = models.TextField(blank=False, null=False)
-    featured = models.BooleanField()
+    featured = models.BooleanField(default=False)
