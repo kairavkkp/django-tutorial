12b158ec751863efc8f524b2b5f3a223c8d7fa10
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-10-07 12:01:40 +0530

    Load data from Database

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+
+def detailed_product_view(request):
+    obj = Product.objects.get(id=1)
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
+    context = {
+        'object': obj
+    }
+    return render(request, 'product/detail.html', context)
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view
-
+from products.views import detailed_product_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
-    path('about/', about_view, name='about')
+    path('about/', about_view, name='about'),
+    path('products/', detailed_product_view, name='products')
 
 ]
