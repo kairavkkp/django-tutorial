f4594a66d2a7eac64143b7658305e43525bf0cd3
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-10-15 15:18:45 +0530

    absolute URL

--- src/products/models.py
+++ src/products/models.py
@@ -9,3 +9,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10000, decimal_places=2)
     summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return f'/products/{self.id}/'
