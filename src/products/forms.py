d52caba0208921bbfc3066471375b419f9106bf0
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-10-15 13:02:50 +0530

    Raw Form WIdgets

--- src/products/forms.py
+++ src/products/forms.py
@@ -13,6 +13,18 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'placeholder': 'Your Product title'
+        }
+    ))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={"class": "new-class-name two",
+                   "rows": 20,
+                   "cols": 80,
+                   'id': 'my-id-for-text-area'
+                   })
+    )
+    price = forms.DecimalField(initial=199.99)
